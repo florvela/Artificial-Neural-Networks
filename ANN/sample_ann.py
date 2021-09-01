288f5ea10dd91287e91e069c2116cc691606dd9e
Florencia Vela <flor@Florencias-MacBook-Pro.local>
2021-09-01 20:55:04 -0300

    encode column

--- ANN/sample_ann.py
+++ ANN/sample_ann.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from sklearn.preprocessing import LabelEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
 
 # Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
@@ -11,3 +13,7 @@ y = dataset.iloc[:, -1].values
 # encode labels. e.g. "Female" is "1, 0" and "Male" is "0, 1"
 le = LabelEncoder()
 X[:, 2] = le.fit_transform(X[:, 2])
+
+# one - hot encoding
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
